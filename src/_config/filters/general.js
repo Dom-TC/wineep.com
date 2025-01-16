3d042d0bd358a8fb7189ef40f85b991ccafc2f13
Dom Chester <dom.chester@me.com>
2025-01-16 00:53:51 +0000

    Update link processing

--- src/_config/filters/general.js
+++ src/_config/filters/general.js
@@ -1,6 +1,25 @@
+import { URL } from "url";
+
 export default {
+    // Remove any trailing index.html from a given URL
     stripIndex: (path) => {
         if (!path) return ''
         return path.replace('/index.html', '/')
     },
+
+    // Get the base domain for a given URL
+    baseDomain: (url) => {
+        try {
+            const parsedUrl = new URL(url);
+            return parsedUrl.hostname.replace(/^www\./, ""); // Remove 'www.' if present
+        } catch (e) {
+            console.error(`Invalid URL: ${url}`);
+            return url; // Fallback to returning the original URL if invalid
+        }
+    },
+
+    // Checks whether a value is an array
+    isArray: (value) => {
+        return Array.isArray(value);
+      }
 }
